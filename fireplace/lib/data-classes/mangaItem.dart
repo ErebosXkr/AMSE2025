c29c7b8b4edcc2246dfc44fec226cbff5b782083
Xkrdve <119463013+Xkrdve@users.noreply.github.com>
2025-02-05 18:47:15 +0100

    data implementation, json and images

--- fireplace/lib/data-classes/mangaItem.dart
+++ fireplace/lib/data-classes/mangaItem.dart
@@ -1,9 +1,10 @@
 class MangaItem {
 
   final String title;
+  final String desc;
+  final String id;
+  final String coverId;
 
-
-  const MangaItem(this.title);
-
+  const MangaItem(this.id, this.title, this.desc, this.coverId);
 
 }
--- fireplace/lib/widgets/itemList.dart
+++ fireplace/lib/widgets/itemList.dart
@@ -1,6 +1,9 @@
 
+import 'package:fireplace/data-classes/mangaItem.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 
 class Itemlist extends StatefulWidget {
   const Itemlist({super.key});
@@ -12,8 +15,22 @@ class Itemlist extends StatefulWidget {
 
 class _ItemListState extends State<Itemlist> {
 
-  var elements = <int>[];
+  var elements = <MangaItem>[];
   var likedElements = <int>[];
+
+  void readData(String path) async {
+    var input = await rootBundle.loadString(path);
+    Map data = jsonDecode(input);
+    for(String key in data.keys) {
+      elements.add(MangaItem(key, data[key]["title"], data[key]["desc"], data[key]["cover_id"]));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    readData("assets/manga-data/data.json");
+  }
 
   @override
   Widget build(BuildContext context) {
